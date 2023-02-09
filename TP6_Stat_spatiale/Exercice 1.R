ed4173f09736ecf85d2cdfeb4d554fa22a2120e4
WolfPackStatMathieu <mathieu.thomassin@eleve.ensai.fr>
2023-02-09 08:41:50 +0000

    initialisation

---
+++ TP6_Stat_spatiale/Exercice 1.R
@@ -0,0 +1,18 @@
+# Exercice 1
+# Chargement des packages
+library(dplyr)
+library(sf)
+library(spdep)
+library(RColorBrewer)
+
+# Import des données
+iris<-st_read("./fonds/iris_franceentiere_2021/iris_franceentiere_2021.shp")
+data<-read.csv2("./data/BASE_TD_FILO_DISP_IRIS_2018.csv",sep=";",dec=".")
+
+# Jointure
+marseille<-iris %>% 
+  filter(substr(depcom,1,3)=="132") %>% 
+  left_join(
+    data %>% select(code=IRIS,DISP_MED18),
+    by="code"
+  )
